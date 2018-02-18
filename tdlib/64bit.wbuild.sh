a460f9d54e3d3bb842cd539792a599df504570f4
isopen <mr_makss@mail.ru>
2018-02-18 16:39:31 +0700

    Update 64bit.wbuild.sh

--- tdlib/64bit.wbuild.sh
+++ tdlib/64bit.wbuild.sh
@@ -1,3 +1,3 @@
 vcpkg --triplet x64-windows install openssl zlib
-cmake .. -DCMAKE_TOOLCHAIN_FILE=…/vcpkg.cmake -A x64
+cmake .. -DCMAKE_TOOLCHAIN_FILE=C:\src\vcpkg\scripts\buildsystems\vcpkg.cmake -A x64
 cmake --build . --config Release
